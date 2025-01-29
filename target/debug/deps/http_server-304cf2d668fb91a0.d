f41de921db79c172f7ae4f421a2448b9f1c0efd6
Chaitany-exe <jeetkrishna3333@gmail.com>
2025-01-29 23:00:19 +0530

    rust server init

---
+++ target/debug/deps/http_server-304cf2d668fb91a0.d
@@ -0,0 +1,5 @@
+/home/frost/personal/code/rust_practice/http_server/target/debug/deps/libhttp_server-304cf2d668fb91a0.rmeta: src/main.rs
+
+/home/frost/personal/code/rust_practice/http_server/target/debug/deps/http_server-304cf2d668fb91a0.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/http_server.d
@@ -0,0 +1,1 @@
+/home/frost/personal/code/rust_practice/http_server/target/debug/http_server: /home/frost/personal/code/rust_practice/http_server/src/main.rs
